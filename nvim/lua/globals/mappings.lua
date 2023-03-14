dda62240d7eae12b8434cadbc18acd8c56535df1
Siddhartha Shrestha <sdhrtivv@gmail.com>
2023-03-14 21:22:28 +0545

    added open float diagnostic

--- nvim/lua/globals/mappings.lua
+++ nvim/lua/globals/mappings.lua
@@ -5,6 +5,7 @@ map("i", "kk", "<Esc>")
 map("n", "<C-n>", ":NvimTreeToggle<CR>")
 
 map("n", "<leader>fm", ":lua vim.lsp.buf.format()<CR>")
+map("n", "<leader>d", ":lua vim.diagnostic.open_float(0,{scope='line',border='single'})<CR>")
 
 map("i", "<C-l>", "<Right>")
 map("i", "<C-h>", "<Left>")
@@ -15,7 +16,7 @@ map('n', "<Tab>", ":bn<CR>")
 map('n', "<leader>x", ":bd<CR>")
 
 if (vim.bo.filetype == "c")
-    then
-        print("wow")
-        map('n', '<F5>', ":!g++ -g % -o %:r <CR>")
-    end
+then
+    print("wow")
+    map('n', '<F5>', ":!g++ -g % -o %:r <CR>")
+end
